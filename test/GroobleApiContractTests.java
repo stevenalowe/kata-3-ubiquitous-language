d635de3d28b64af81890fce52f8a924b9ae44492
Steven A Lowe <slowe@thoughtworks.com>
2016-02-04 17:09:02 -0800

    added files for step 4

--- test/GroobleApiContractTests.java
+++ test/GroobleApiContractTests.java
@@ -7,15 +7,41 @@ import org.junit.Test;
 public class GroobleApiContractTests {
     private GroobleApi _groobleApi = new GroobleApi();
     @Test
-    public void HappyPathHitTest() {
-        //in which we do everything right
+    public void HappyPathHitTest() throws Exception {
+        //initially it's empty and wasn't hit
+        Assert.assertTrue(_groobleApi.empty());
+        Assert.assertFalse(_groobleApi.washit());
+        //so we load it
+        _groobleApi.load();
+        //and now it's not empty
+        Assert.assertFalse(_groobleApi.empty());
+        //and still wasn't hit
+        Assert.assertFalse(_groobleApi.washit());
+        //so we hit it
+        _groobleApi.hit();
+        //now it was hit
+        Assert.assertTrue(_groobleApi.washit());
+        //but it's still not empty
+        Assert.assertFalse(_groobleApi.empty());
+        //reset it
+        _groobleApi.reset();
+        //and now it's empty
+        Assert.assertTrue(_groobleApi.empty());
+        //and wasn't hit
+        Assert.assertFalse(_groobleApi.washit());
     }
     @Test(expected = Exception.class)
     public void FailurePathReHitTest() throws Exception {
-        //in which we trigger a rehit exception
+        //in which we trigger a hit exception
+        //load and hit it twice to cause exception
+        _groobleApi.load();
+        _groobleApi.hit();
+        _groobleApi.hit();
     }
     @Test(expected = Exception.class)
     public void FailurePathEmptyHitTest() throws Exception {
         //in which we trigger a hit exception on an empty machine
+        Assert.assertTrue(_groobleApi.empty());
+        _groobleApi.hit();
     }
 }
